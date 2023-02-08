cf371ee4bd5f06e53eb2ffb2410df4ee587ff7e0
alexandraep <alexandra.pirjol14@gmail.com>
2023-02-08 09:07:53 +0000

    started using auth0

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,13 +1,33 @@
 import Link from "next/link";
 
 import styles from "@/styles/Navigation.module.scss"
+import {useAuth0} from "@auth0/auth0-react";
 
 
 export default function () {
+    const {
+        isAuthenticated,
+        loginWithRedirect,
+        logout
+    } = useAuth0();
+
+
+
     return (
         <nav className={styles.navigation}>
             <Link href={'/'}>Home</Link>
             <Link href={'/clients'}>Clients</Link>
+            {
+                !isAuthenticated ? (
+                    <button onClick={e => loginWithRedirect()}>Log in</button>
+                ) : (
+                    <button onClick={() => {
+                        // @ts-ignore
+                        logout({ returnTo: window.location.origin });
+                    }}>Log out</button>
+                )
+            }
+
         </nav>
     )
 }
